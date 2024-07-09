65afce2c28be2b58c8cde6c99771098adc5c8aec
Nidhi <nidhi.sarsan@gmail.com>
2024-07-09 15:45:21 +0530

    pushing my entire java basic code

---
+++ Class_01.java
@@ -0,0 +1,18 @@
+package package_01;
+
+import package_02.Class_02;
+
+public class Class_01 extends Class_02 
+
+{
+	public static void main(String[] args) 
+	{
+		Class_01 c1=new Class_01();
+		c1.add();//public Access specifier in class_01
+		c1.mul();//protected access specifier
+		
+		
+		
+	}
+
+}
