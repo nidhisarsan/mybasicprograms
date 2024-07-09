71b369c988b1f2f4022ba2117dfc52b6be7e2017
Nidhi <nidhi.sarsan@gmail.com>
2024-07-09 15:49:18 +0530

    pushing my entire java basic code

---
+++ Student.java
@@ -0,0 +1,14 @@
+package Package_1;
+
+public class Student {
+	public static void main(String[] args) {
+		Teacher t1=new Teacher();
+		t1.add();
+		t1.mul();
+		t1.div();
+		
+		
+		
+	}
+
+}
---
+++ Teacher.java
@@ -0,0 +1,26 @@
+package Package_1;
+
+import inheritance_java.Access_Specifier_Method_1;
+
+public class Teacher {
+	public void add()
+	{
+		
+	}
+	private void sub()
+	{
+		
+	}
+	protected void mul()
+	{
+		
+	}
+	void div()
+	{
+		
+	}
+	public static void main(String[] args) {
+		  
+		
+
+}
